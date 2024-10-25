693637bbba028c0afceb7c3f82b05d64094ceb13
Vanesa-cmyk <vanesatique05@gmail.com>
2024-10-25 14:30:15 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,1 +1,11 @@
+<?php
+$Identificacion=$_POST['Identificacion'];
+$NomApellidos=$_POST['Cargo'];
+$Cargo=$_Post['Cargo'];
+$Salario=$_POST['Salario'];
+echo "la identificaion del empleado es: ".Idenficacion;
+echo "Los nombres y apellidos del empleado son:".NomApellidos;
+echo "El cargo del empleado es: ".Cargo;
+echo "El salario del empleado es: ".Salario;
+?>
 
